0e098504e8565241d86f6cc361f1d145119be8e1
constantine47 <konstantin.anikiev@justwebit.ru>
2019-12-23 12:13:18 +0300

    update eraser script

--- cron.py
+++ cron.py
@@ -6,7 +6,6 @@ class Eraser:
     def __init__(self):
         self.delay = 300
         self.files = {}
-        self.dirs = {}
 
     def run(self):
         while True:
@@ -25,19 +24,9 @@ class Eraser:
                         self.files[path] = 60
                         print('Added new file {}'.format(file))
 
-                # find new directories
-                for dir in dirs:
-                    path = address+'/'+dir
-                    if path not in self.dirs and len(os.listdir(path)) == 0:
-                        self.dirs[path] = 60
-                        print('Added new dir {}'.format(dir))
-
     def count_values(self):
         if self.files.values():
             for val in self.files.values():
-                val -= 5
-        if self.dirs.values():
-            for val in self.dirs.values():
                 val -= 5
 
     def remove_dead(self):
@@ -47,13 +36,6 @@ class Eraser:
                 os.remove(key)
                 del self.files[key]
                 print('File {} was removed'.format(key))
-
-        # remove empty folders
-        for key, val in self.dirs.items():
-            if val <= 0:
-                os.rmdir(dir)
-                del self.dirs[dir]
-                print('Dir {} was removed'.format(dir))
 
 
 if __name__ == '__main__':
