2d9a512af47e4587f56bc66597cfc5663d4667f1
constantine47 <konstantin.anikiev@justwebit.ru>
2020-05-08 09:58:20 +0300

    disable reverse proxy path

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_dropzone import Dropzone
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
+# from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
 
 from app.config import Config
 
@@ -10,8 +10,8 @@ app = Flask(__name__, static_url_path='/static')
 
 app.config.from_object(Config)
 
-app.config['REVERSE_PROXY_PATH'] = '/converter'
-ReverseProxyPrefixFix(app)
+# app.config['REVERSE_PROXY_PATH'] = '/converter'
+# ReverseProxyPrefixFix(app)
 
 login = LoginManager(app)
 dropzone = Dropzone(app)
